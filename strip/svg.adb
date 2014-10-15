02fc5aa04e14e543206f2da750a9960d36e323d6
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-10-15 14:07:18 +0200

    Correction de quelques erreurs de linkage de svg.ad*

--- strip/svg.adb
+++ strip/svg.adb
@@ -1,55 +1,65 @@
-with Ada.Text_IO, Ada.Float_Text_IO, Ada.Integer_Text_IO;
-use Ada.Text_IO, Ada.Float_Text_IO, Ada.Integer_Text_IO;
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use Ada.Text_IO, Ada.Integer_Text_IO;
 
 package body Svg is
 
-	Display_Width, Display_Height : Float;
+   Display_Width, Display_Height : Natural;
 
-	--  procedure Svg_Line(P1, P2 : Point ; C : Color) is
-	--  begin
-	--  	Put("<line x1=""");
-	--  	Put(P1.X);
-	--  	Put(""" y1=""");
-	--  	Put(P1.Y);
-	--  	Put(""" x2=""");
-	--  	Put(P2.X);
-	--  	Put(""" y2=""");
-	--  	Put(P2.Y);
-	--  	Put(""" style=""stroke:");
-	--  	case C is
-	--  		when Red => Put("rgb(255,0,0)");
-	--  		when Green => Put("rgb(0,255,0)");
-	--  		when Blue => Put("rgb(0,0,255)");
-	--  		when Black => Put("rgb(0,0,0)");
-	--  	end case;
-	--  	Put_Line(";stroke-width:0.2""/>");
-	--  end Svg_Line;
+   --  procedure Svg_Line(P1, P2 : Point ; C : Color) is
+   --  begin
+   --      Put("<line x1=""");
+   --      Put(P1.X);
+   --      Put(""" y1=""");
+   --      Put(P1.Y);
+   --      Put(""" x2=""");
+   --      Put(P2.X);
+   --      Put(""" y2=""");
+   --      Put(P2.Y);
+   --      Put(""" style=""stroke:");
+   --      case C is
+   --              when Red => Put("rgb(255,0,0)");
+   --              when Green => Put("rgb(0,255,0)");
+   --              when Blue => Put("rgb(0,0,255)");
+   --              when Black => Put("rgb(0,0,0)");
+   --      end case;
+   --      Put_Line(";stroke-width:0.2""/>");
+   --  end Svg_Line;
 
-	--  procedure Svg_Circle(C : Point ; R : Float) is
-	--  begin
-	--  	Put("<circle cx=""");
-	--  	Put(C.X);
-	--  	Put(""" cy=""");
-	--  	Put(C.Y);
-	--  	Put(""" r=""");
-	--  	Put(R);
-	--  	Put_Line(""" stroke=""black"" stroke-width=""0.1"" fill=""none""/>");
-	--  end Svg_Circle;
+   --  procedure Svg_Circle(C : Point ; R : Float) is
+   --  begin
+   --      Put("<circle cx=""");
+   --      Put(C.X);
+   --      Put(""" cy=""");
+   --      Put(C.Y);
+   --      Put(""" r=""");
+   --      Put(R);
+   --      Put_Line(""" stroke=""black"" stroke-width=""0.1"" fill=""none""/>");
+   --  end Svg_Circle;
 
-	procedure Svg_Header(Width, Height : Integer) is
-	begin
-		Put("<svg width=""");
-		Put(Width);
-		Put(""" height=""");
-		Put(Height);
-		Put_Line(""">");
-		Display_Width := Float(Width);
-		Display_Height := Float(Height);
-	end Svg_Header;
+   procedure Svg_Header(FichierSvg: String;
+                        Width, Height : Natural) is
+   begin
+      --  Put("<svg width=""");
+      --  Put(Width);
+      --  Put(""" height=""");
+      --  Put(Height);
+      --  Put_Line(""">");
+      --  Display_Width := Float(Width);
+      --  Display_Height := Float(Height);
+      null;
+   end Svg_Header;
 
-	procedure Svg_Footer is
-	begin
-		Put_Line("</svg>");
-	end Svg_Footer;
+   procedure Svg_Footer (FichierSvg : String) is
+   begin
+      Put_Line("</svg>");
+   end Svg_Footer;
+
+   procedure Sauvegarde (FichierSvg : String;
+                         Objets : Tableau_Objets;
+                         Largeur_Ruban : Natural;
+                         Hauteur_Ruban : Natural) is
+   begin
+      null;
+   end Sauvegarde;
 
 end Svg;
--- strip/svg.ads
+++ strip/svg.ads
@@ -1,23 +1,26 @@
+with Objet_Packing;
+use Objet_Packing;
 
 package Svg is
-   --	type Color is (Red, Green, Blue, Black);
-   
+   --   type Color is (Red, Green, Blue, Black);
+
    -- -> DANS ADB
-   
+
    -- ! A appeler avant toute ecriture dans le fichier svg !
    -- garantit : Insere le header svg dans FichierSvg.
-   procedure Svg_Header (FichierSvg : String);
-   
+   procedure Svg_Header (FichierSvg : String;
+                         Width, Height : Natural);
+
    -- ! A appeler pour clore le fichier svg !
    -- garantit : Insere le footer svg dans FichierSvg.
    procedure Svg_Footer (FichierSvg : String);
-   
+
    ----------------------------------------
-   
+
    -- Sauvegarde la position des objets dans le fichier svg.
-   procedure Sauvegarde (FichierSvg : String,
-			   Objets : Tableau_Objets,
-			   Largeur_Ruban : Natural
-			   Hauteur_Ruban : Natural);
-   
+   procedure Sauvegarde (FichierSvg : String;
+                         Objets : Tableau_Objets;
+                         Largeur_Ruban : Natural;
+                         Hauteur_Ruban : Natural);
+
 end Svg;
