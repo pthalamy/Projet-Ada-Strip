95a3224804144c8a946c6a50db30c78e8a3f0097
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-10-15 14:06:33 +0200

    Implementation de la lecture de l entete

--- strip/parseur.adb
+++ strip/parseur.adb
@@ -1,30 +1,51 @@
 
-with Ada.Text_IO;
-use Ada.Text_IO;;
+with Ada.Text_IO, Ada.Integer_Text_IO;
+use Ada.Text_IO, Ada.Integer_Text_IO;
 
-package body Parseur is 
-   
-   procedure Lecture_En_Tete (FichierEntree : in String, 
-				Nombre_Objets : out Natural, 
-				Largeur_Ruban : out Natural) is
+package body Parseur is
+
+   procedure Lecture_En_Tete (Nom_Fichier : in String;
+                              Nombre_Objets : out Natural;
+                              Largeur_Ruban : out Natural) is
       Fichier : File_Type;
-   begin 
+   begin
       -- Ouverture du fichier
       Open (File => Fichier,
-	    Mode => In_File;,
-	    Name => FichierEntree);
-      
+            Mode => In_File,
+            Name => Nom_Fichier);
+
       -- Lecture du nombre d'objets
       Get (Fichier, Nombre_Objets);
-      
+
       -- Lecture de la largeur du ruban
-      Get (Largeur_Ruban);
- 
+      Get (Fichier, Largeur_Ruban);
+
    end Lecture_En_Tete;
-   
-   procedure Lecture (FichierEntree : String, Objets : Tableau_Objets) is
+
+   procedure Lecture (Nom_Fichier : in String; Objets : out Tableau_Objets) is
+      Fichier : File_Type;
+      Index_Objet, Largeur_Objet, Hauteur_Objet : Natural;
    begin
-      
+      -- Ouverture du fichier
+      Open (File => Fichier,
+            Mode => In_File,
+            Name => Nom_Fichier);
+
+      -- Saut des deux premieres lignes d'entete
+      Skip_Line(File => Fichier, Spacing => 2);
+
+      -- Pour tous les objets du fichier, en extraire les informations et les stocker
+      -- dans le tableau a l'index donne par leur id + 1.
+      -- Format : Index Largeur Hauteur
+      while not End_Of_File (Fichier) loop
+         Get (Fichier, Index_Objet);
+         Get (Fichier, Largeur_Objet);
+         Get (Hauteur_Objet);
+
+         Tableau_Objets(Index_Objet + 1).Largeur = Largeur_Objet;
+         Tableau_Objets(Index_Objet + 1).Hauteur = Hauteur_Objet;
+      End loop;
+
    end Lecture;
-   
+
 end Parseur;
--- strip/parseur.ads
+++ strip/parseur.ads
@@ -1,17 +1,21 @@
+with Objet_Packing;
+use Objet_Packing;
 
 package Parseur is
-   
-   -- requiert : FichierEntree nom de fichier existant et valide.
-   -- garantit : Nombre_Objets et Largeur_Ruban fixes a leur valeur 
+
+   Erreur_Lecture_Benchmark : exception;
+
+   -- requiert : Nom_Fichier nom de fichier existant et valide.
+   -- garantit : Nombre_Objets et Largeur_Ruban fixes a leur valeur
    -- correspondante dans le fichier lu.
-   procedure Lecture_En_Tete (FichierEntree : String, 
-				Nombre_Objets : Natural, 
-				Largeur_Ruban : Natural);
-   
-   
-   -- requiert : FichierEntree nom de fichier existant et valide.
+   procedure Lecture_En_Tete (Nom_Fichier : in String;
+                              Nombre_Objets : out Natural;
+                              Largeur_Ruban : out Natural);
+
+
+   -- requiert : Nom_Fichier nom de fichier existant et valide, Objets initialise.
    -- garantit : Objets rempli d'objets de dimensions contenues dans le fichier lu.
-   procedure Lecture (FichierEntree : String,
-			Objets : Tableau_Objets);
-   
+   procedure Lecture (Nom_Fichier : in String;
+                      Objets : out Tableau_Objets);
+
 end Parseur;
