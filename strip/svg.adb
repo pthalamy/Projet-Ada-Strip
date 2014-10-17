6bed6561106e3e4e2b442ab0ff268395e51e6350
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-10-17 13:41:25 +0200

    Ajout de toutes les couleurs de larc en ciel aux couleurs existantes

--- strip/svg.adb
+++ strip/svg.adb
@@ -5,23 +5,27 @@ with Objet_Packing;
 use Objet_Packing;
 
 package body Svg is
-   
-   type Color is (Rouge, Vert, Bleu, Noir, Blanc);
-   
+
+   type Color is (Violet, Indigo, Bleu, Vert, Jaune, Orange, Rouge, Noir, Blanc);
+
    Display_Width, Display_Height : Natural;
    Fichier_Svg : File_Type;
-   
+
    function Code_Couleur (C : Color) return String is
    begin
       case C is
-	 when Rouge => return "rgb(255,0,0)";
-	 when Vert  => return "rgb(0,255,0)";
-	 when Bleu  => return "rgb(0,0,255)";
-	 when Noir  => return "rgb(0,0,0)";
-	 when Blanc => return "rgb(255,255,255)";
+         when Violet  => return "rgb(255,0,255)";
+         when Indigo  => return "rgb(111,0,255)";
+         when Bleu  => return "rgb(0,0,255)";
+         when Vert  => return "rgb(0,255,0)";
+         When Jaune => return "rgb(0,255,255)";
+         When Jaune => return "rgb(255,165,0)";
+         when Rouge => return "rgb(255,0,0)";
+         when Noir  => return "rgb(0,0,0)";
+         when Blanc => return "rgb(255,255,255)";
       end case;
-   end Code_Couleur;   
-      
+   end Code_Couleur;
+
    -- ! A appeler avant toute ecriture dans le fichier svg !
    -- garantit : Insere le header svg dans Fichier_Svg.
    procedure Svg_Header (Largeur, Hauteur : Natural) is
@@ -31,22 +35,22 @@ package body Svg is
       Put (Fichier_Svg, """ height=""");
       Put (Fichier_Svg, Hauteur);
       Put_Line (Fichier_Svg, """>");
-      
+
       Display_Width := Largeur;
       Display_Height := Hauteur;
    end Svg_Header;
-   
-   
+
+
    -- ! A appeler pour clore le fichier svg !
    -- garantit : Insere le footer svg dans Fichier_Svg.
    procedure Svg_Footer is
    begin
       Put_Line (Fichier_Svg, "</svg>");
    end Svg_Footer;
-   
+
    -- Dessing une ligne A -- B
    procedure Svg_Line (A, B : Point;
-		       C : Color) 
+                       C : Color)
    is
    begin
       Put (Fichier_Svg, "<line x1=""");
@@ -58,21 +62,21 @@ package body Svg is
       Put (Fichier_Svg, """ y2=""");
       Put (Fichier_Svg, B.Y);
       Put (Fichier_Svg, """ style=""stroke:");
-      
+
       Put (Fichier_Svg, Code_Couleur (C));
-            
+
       Put_Line (Fichier_Svg, ";stroke-width:1""/>");
    end Svg_Line;
-   
-   
+
+
    -- Dessine un rectangle de la forme:
    --              A/O ---- B
    --               |      |
    --               D ---- C
    -- et de couleur C.
    procedure Svg_Rectangle (O : Point;
-			    Largeur, Hauteur : Natural;
-			    C : Color) 
+                            Largeur, Hauteur : Natural;
+                            C : Color)
    is
    begin
       Put (Fichier_Svg, "<rect ");
@@ -80,10 +84,10 @@ package body Svg is
       Put (Fichier_Svg, "y=""" & Integer'Image(O.Y) & """ ");
       Put (Fichier_Svg, "width=""" & Integer'Image(Largeur) & """ ");
       Put (Fichier_Svg, "height=""" & Integer'Image(Hauteur) & """ ");
-      Put (Fichier_Svg, "style=""fill:" & Code_Couleur (C) & ";" 
-	     & "stroke-width:0;"
-	     & "stroke:" & Code_Couleur (Noir));
-      Put_Line (Fichier_Svg, """/>");   
+      Put (Fichier_Svg, "style=""fill:" & Code_Couleur (C) & ";"
+             & "stroke-width:0;"
+             & "stroke:" & Code_Couleur (Noir));
+      Put_Line (Fichier_Svg, """/>");
    end Svg_Rectangle;
 
    procedure Sauvegarde (Nom_Fichier_Svg : String;
@@ -95,11 +99,11 @@ package body Svg is
       Open (File => Fichier_Svg,
             Mode => Out_File,
             Name => Nom_Fichier_Svg);
-      
+
       Svg_Header (Largeur_Ruban, Hauteur_Ruban);
-      
+
       Svg_Rectangle ((10, 10), 150, 75, Rouge);
-	
+
       Svg_Footer;
    end Sauvegarde;
 
