c3244196963bf71504caf2843ea5ae209191e6a3
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-10-19 22:05:53 +0200

    Rename Origine en Position, Put deviens Put_Line

--- strip/objet_packing.adb
+++ strip/objet_packing.adb
@@ -2,7 +2,16 @@ with Ada.Integer_Text_IO, Ada.Text_IO;
 use Ada.Integer_Text_IO, Ada.Text_IO;
 
 package body Objet_Packing is
-
+   
+   -- SETTERS
+   
+   procedure Set_Index (This : in out Objet;
+			Index : in Natural)
+   is
+   begin
+      This.Index := Index;
+   end Set_Index;
+   
    procedure Set_Largeur (This : in out Objet;
                           Largeur : in Natural)
    is
@@ -16,18 +25,37 @@ package body Objet_Packing is
    begin
       This.Hauteur := Hauteur;
    end Set_Hauteur;
-
-
+   
+   procedure Set_Position (This : in out Objet;
+                          Position : in Point)
+   is
+   begin
+      This.Position := Position;
+   end Set_Position;      
+   
+   -- GETTERS
+   
+   function Get_Index (This : Objet) return Natural is
+   begin
+      return This.Index;
+   end Get_Index;
+   
    function Get_Largeur (This : Objet) return Natural is
    begin
       return This.Largeur;
    end Get_Largeur;
 
-
    function Get_Hauteur (This : Objet) return Natural is
    begin
       return This.Hauteur;
    end Get_Hauteur;
+   
+   function Get_Position (This : Objet) return Point is
+   begin
+      return This.Position;
+   end Get_Position;
+   
+   -- PUT
    
    procedure Put (P : in Point) is
    begin
@@ -35,21 +63,21 @@ package body Objet_Packing is
       Put (P.Y);
    end Put;
    
-   procedure Put (Obj : in Objet) is
+   procedure Put_Line (Obj : in Objet) is
    begin
+      Put (Get_Index (Obj));
       Put (Get_Largeur (Obj));
       Put (Get_Hauteur (Obj));
+      Put (Get_Position (Obj));
       New_Line;
-   end Put;
+   end Put_Line;
 
-   procedure Put (Tab : in Tableau_Objets;
-                  Taille : in Natural)
+   procedure Put_Line (Tab : in Tableau_Objets)
    is
    begin
-      for I in 1..Taille loop
-         Put (I);
-         Put (Tab(I));
+      for I in Tab'Range loop
+         Put_Line (Tab(I));
       end loop;
-   end Put;
+   end Put_Line;
    
 end Objet_Packing;
--- strip/objet_packing.ads
+++ strip/objet_packing.ads
@@ -8,27 +8,37 @@ package Objet_Packing is
 
    type Objet is private;
    type Tableau_Objets is array (Integer range <>) of Objet;
-
+   
+   procedure Set_Index (This : in out Objet;
+			Index : in Natural);
+   
    procedure Set_Largeur (This : in out Objet;
                           Largeur : in Natural);
 
    procedure Set_Hauteur (This : in out Objet;
                           Hauteur : in Natural);
-
+   
+   procedure Set_Position (This : in out Objet;
+			  Position : in Point);
+   
+   function Get_Index (This : Objet) return Natural;
+   
    function Get_Largeur (This : Objet) return Natural;
 
    function Get_Hauteur (This : Objet) return Natural;
+   
+   function Get_Position (This : Objet) return Point;
 
    -- Affiche le contenu des objets d'un tableau
-   procedure Put (Tab : in Tableau_Objets;
-                  Taille : in Natural);
+   procedure Put_Line (Tab : in Tableau_Objets);
    
 private
 
    type Objet is record
+      Index : Natural;
       Largeur : Natural;
       Hauteur : Natural;
-      Origine : Point;
+      Position : Point; -- Top-Left Corner
    end record;
 
 end Objet_Packing;
