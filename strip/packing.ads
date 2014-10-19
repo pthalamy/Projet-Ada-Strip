c4b28621969185713f7343cfd36718f3a61705a1
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-10-19 22:06:23 +0200

    Idem last commit

--- strip/packing.ads
+++ strip/packing.ads
@@ -1,4 +1,10 @@
+with Objet_Packing;
+use Objet_Packing;
 
 package Packing is
-
+   
+   procedure Next_Fit_Decreasing_Height (Objets : in out Tableau_Objets;
+					 Largeur_Ruban : in Natural;
+					 Hauteur_Ruban : out Natural);
+   
 end Packing;
