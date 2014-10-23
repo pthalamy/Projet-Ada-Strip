b515027d6c7f74a789cf2e0f95585acae1689d6d
Benji118 <benjamin.lebit@yahoo.fr>
2014-10-23 17:37:00 +0200

    Update packing.ads

--- strip/packing.ads
+++ strip/packing.ads
@@ -4,8 +4,8 @@ use Objet_Packing;
 package Packing is
    
    -- Requiert : Objets un Tableau_Objets non vide
-   -- Garantit : Objets trier par ordre decroissant de la hauteur des ses objets
-   --            Chaque Objet.Position assigne a une valeur de type point
+   -- Garantit : Objets trier par ordre decroissant de la hauteur de ses objets
+   --            Chaque Objet.Position assigné à une valeur de type point
    procedure Next_Fit_Decreasing_Height (Objets : in out Tableau_Objets;
 					 Largeur_Ruban : in Natural;
 					 Hauteur_Ruban : out Natural);
