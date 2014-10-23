28e9dde4141e4046b76fd0834a943e98d2693617
Benji118 <benjamin.lebit@yahoo.fr>
2014-10-23 15:47:13 +0200

    Create packing.ads
    packing benji

---
+++ packing.ads
@@ -0,0 +1,8 @@
+with Objet_Packing;
+use Objet_Packing;
+
+package Packing is
+   
+   procedure Next_Fit_Decreasing_Height(Objets : in out Tableau_Objets ; Largeur_Ruban : Natural ; Hauteur_Ruban : in out Natural);
+   
+   end Packing;
