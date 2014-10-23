f9849b08da22c7ea684f2dd3fe39fa970a42ef49
Benji118 <benjamin.lebit@yahoo.fr>
2014-10-23 17:39:13 +0200

    Update parseur.ads

--- strip/parseur.ads
+++ strip/parseur.ads
@@ -5,18 +5,18 @@ package Parseur is
    
    Erreur_Lecture_Benchmark : exception;
    
-   -- requiert : Nom_Fichier nom de fichier existant et bien formatte.
-   --            (Largeur et nombre d'elements non nuls)
+   -- requiert : Nom_Fichier nom de fichier existant et bien formatté.
+   --            (Largeur et nombre d'éléments non nuls)
    -- garantit : Lecture et stockage du nombre d'objets 
-   --            et de la largeur du ruban dans le fichier d'entree.
+   --            et de la largeur du ruban dans le fichier d'entrée.
    procedure Lecture_En_Tete (Nom_Fichier : in String;
                               Nombre_Objets : out Natural;
                               Largeur_Ruban : out Natural);
 
 
-   -- requiert : Nom_Fichier nom de fichier existant et bien formatte,
-   --            Objets bien indexes,
-   --            Objets Tableau_Objets initialise.
+   -- requiert : Nom_Fichier nom de fichier existant et bien formatté,
+   --            Objets bien indexés,
+   --            Objets Tableau_Objets initialisé.
    -- garantit : Objets rempli d'objets de dimension et indice
    --            lus dans le fichier.
    procedure Lecture (Nom_Fichier : in String;
