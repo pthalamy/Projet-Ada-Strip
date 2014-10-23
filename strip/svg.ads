0ce680810767a29353ddb3db380e529b786669b4
Benji118 <benjamin.lebit@yahoo.fr>
2014-10-23 17:40:55 +0200

    Update svg.ads

--- strip/svg.ads
+++ strip/svg.ads
@@ -6,7 +6,7 @@ package Svg is
    -- Requiert : Nom_Fichier_Svg nom de fichier valide
    -- Garantit : Un fichier de nom Nom_Fichier_Svg en sortie
    --            contenant une image svg de taille Largeur_Ruban * Hauteur_Ruban
-   --            et une representation en couleur de chaque element de Objets.
+   --            et une représentation en couleur de chaque élément de Objets.
    procedure Sauvegarde (Nom_Fichier_Svg : in String;
                          Objets : in Tableau_Objets;
                          Largeur_Ruban : in Natural;
